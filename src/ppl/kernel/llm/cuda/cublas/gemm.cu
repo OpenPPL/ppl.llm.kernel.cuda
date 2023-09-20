703ab68dde0c2e6e06421dafd4456f9177ecd249
Jianfei Wang <905787410@qq.com>
2023-09-20 22:13:20 +0800

    cuda-add: a-N b-T int8 gemm based on cublas (#9)
    Co-authored-by: wangjianfei <wangjianfei@sensetime.com>

--- src/ppl/kernel/llm/cuda/cublas/gemm.cu
+++ src/ppl/kernel/llm/cuda/cublas/gemm.cu
@@ -147,4 +147,111 @@ ppl::common::RetCode gemm(
     return ppl::common::RC_SUCCESS;
 }
 
+ppl::common::RetCode int8_gemm(
+    const cudaStream_t stream,
+    const cublasLtHandle_t& cublaslt_handle,
+    const cublasLtMatmulAlgo_t* algo,
+    const bool transa, // must be false
+    const int64_t lda, // transa ? M : K;
+    const ppl::common::datatype_t typea, // int8
+    const void* A, // int8
+    const bool transb, // must be true
+    const int64_t ldb, // transb ? K : N;
+    const ppl::common::datatype_t typeb, // int8
+    const void* B, // int8
+    const void* bias, // int32
+    const int64_t M,
+    const int64_t N,
+    const int64_t K,
+    const int32_t alpha, // int32-C need
+    const int32_t beta, // int32-C need
+    const int64_t workspace_size,
+    void* workspace,
+    const int64_t ldc, // N
+    const ppl::common::datatype_t typec, // int32
+    void* C) // int32
+{
+    if (typea != ppl::common::DATATYPE_INT8) {
+        LOG(ERROR) << "only support int8 A matrix";
+        return ppl::common::RC_UNSUPPORTED;
+    }
+    if (typeb != ppl::common::DATATYPE_INT8) {
+        LOG(ERROR) << "only support int8 B matrix";
+        return ppl::common::RC_UNSUPPORTED;
+    }
+    if (typec != ppl::common::DATATYPE_INT32) {
+        LOG(ERROR) << "only support int32 C matrix";
+        return ppl::common::RC_UNSUPPORTED;
+    }
+
+    cublasLtMatmulDesc_t operationDesc = nullptr;
+    cublasLtMatrixLayout_t Adesc = nullptr, Bdesc = nullptr, Cdesc = nullptr;
+
+    cublasOperation_t cublas_transa = transa == true ? CUBLAS_OP_T : CUBLAS_OP_N;
+    cublasOperation_t cublas_transb = transb == true ? CUBLAS_OP_T : CUBLAS_OP_N;
+    cudaDataType_t scaleType = CUDA_R_32I;
+    cudaDataType_t abType = CUDA_R_8I;
+    cudaDataType_t cType = CUDA_R_32I;
+
+#if (CUDART_VERSION >= 11000)
+    cublasComputeType_t computeType = CUBLAS_COMPUTE_32I;
+    CUBLAS_CHECK_RC(cublasLtMatmulDescCreate(&operationDesc, computeType, scaleType));
+#else
+    cudaDataType_t computeType = scaleType;
+    CUBLAS_CHECK_RC(cublasLtMatmulDescCreate(&operationDesc, computeType));
+#endif
+
+    // exchange A & B to col-major
+    CUBLAS_CHECK_RC(cublasLtMatmulDescSetAttribute(operationDesc, CUBLASLT_MATMUL_DESC_TRANSA, &cublas_transb, sizeof(cublas_transb)));
+    CUBLAS_CHECK_RC(cublasLtMatmulDescSetAttribute(operationDesc, CUBLASLT_MATMUL_DESC_TRANSB, &cublas_transa, sizeof(cublas_transa)));
+
+#if (CUDART_VERSION >= 11000)
+    if (bias != nullptr) {
+        cublasLtEpilogue_t epilogue = CUBLASLT_EPILOGUE_BIAS;
+        CUBLAS_CHECK_RC(cublasLtMatmulDescSetAttribute(operationDesc, CUBLASLT_MATMUL_DESC_EPILOGUE, &epilogue, sizeof(epilogue)));
+        CUBLAS_CHECK_RC(cublasLtMatmulDescSetAttribute(operationDesc, CUBLASLT_MATMUL_DESC_BIAS_POINTER, &bias, sizeof(void*)));
+    }
+#endif
+    // create matrix descriptors, we are good with the details here so no need to set any extra attributes
+    CUBLAS_CHECK_RC(cublasLtMatrixLayoutCreate(&Adesc, abType, cublas_transa == CUBLAS_OP_N ? K : M, cublas_transa == CUBLAS_OP_N ? M : K, lda));
+    CUBLAS_CHECK_RC(cublasLtMatrixLayoutCreate(&Bdesc, abType, cublas_transb == CUBLAS_OP_N ? N : K, cublas_transb == CUBLAS_OP_N ? K : N, ldb));
+    CUBLAS_CHECK_RC(cublasLtMatrixLayoutCreate(&Cdesc, cType, N, M, ldc));
+
+    CUBLAS_CHECK_RC(cublasLtMatmul(
+        cublaslt_handle,
+        operationDesc,
+        (const void*)(&alpha),
+        B,
+        Bdesc,
+        A,
+        Adesc,
+        (const void*)(&beta),
+        C,
+        Cdesc,
+        C,
+        Cdesc,
+        nullptr,
+        workspace,
+        workspace_size,
+        stream));
+
+#if (CUDART_VERSION < 11000)
+    if (bias != nullptr) {
+        const int64_t num_elems = M * N;
+        const int64_t block_size = 128;
+        const int64_t blocks = (num_elems + block_size - 1) / block_size;
+        dim3 grid_size(blocks, 1, 1);
+        cublas_gemm_add_bias_kernel<int32_t><<<grid_size, block_size, 0, stream>>>(M * N, N, (int32_t*)C, (const int32_t*)bias);
+    }
+#endif
+
+    // descriptors are no longer needed as all GPU work was already enqueued
+    if (Cdesc) CUBLAS_CHECK_RC(cublasLtMatrixLayoutDestroy(Cdesc));
+    if (Bdesc) CUBLAS_CHECK_RC(cublasLtMatrixLayoutDestroy(Bdesc));
+    if (Adesc) CUBLAS_CHECK_RC(cublasLtMatrixLayoutDestroy(Adesc));
+    if (operationDesc) CUBLAS_CHECK_RC(cublasLtMatmulDescDestroy(operationDesc));
+    
+    return ppl::common::RC_SUCCESS;
+}
+
 }}}}}
