66f00c995199642f51dd456268a4fa1153f74338
alcanderian <alcanderian@gmail.com>
2023-12-05 22:42:34 +0800

    [opy] optimize db.kvcache db.decode_attn  db.rotemb

--- src/ppl/kernel/llm/cuda/pmx/rotary_position_embedding.cu
+++ src/ppl/kernel/llm/cuda/pmx/rotary_position_embedding.cu
@@ -202,59 +202,62 @@ ppl::common::RetCode rotary_position_embedding(
     return ppl::common::RC_SUCCESS;
 }
 
+struct dynamic_batching_rotary_position_embedding_kernel_param {
+    half2 *query;
+    half2 *key;
+    int64_t* start_pos;
+    int64_t* seqstarts;
+    float theta;
+    int64_t rotary_dim;
+    int64_t num_heads;
+    int64_t num_key_heads;
+    int64_t head_dim;
+    int64_t query_stride_s;
+    int64_t key_stride_s;
+    int64_t rotated_query_stride_s;
+    int64_t rotated_key_stride_s;
+    half2 *rotated_query;
+    half2 *rotated_key;
+};
+
+template<int32_t TPB, bool bypass_key>
 __global__
 void dynamic_batching_rotary_position_embedding_kernel(
-    const half2 *query,
-    const half2 *key,
-    const int64_t* start_pos,
-    const int64_t* seqstarts,
-    const float theta,
-    const int32_t bypass_key,
-    const int64_t rotary_dim,
-    const int64_t num_heads,
-    const int64_t num_key_heads,
-    const int64_t head_dim,
-    const int64_t query_stride_s,
-    const int64_t key_stride_s,
-    const int64_t rotated_query_stride_s,
-    const int64_t rotated_key_stride_s,
-    half2 *rotated_query,
-    half2 *rotated_key)
+    dynamic_batching_rotary_position_embedding_kernel_param p)
 {
-    if (blockIdx.y < seqstarts[blockIdx.x + 1] - seqstarts[blockIdx.x]) {
+    if (blockIdx.y < p.seqstarts[blockIdx.x + 1] - p.seqstarts[blockIdx.x]) {
         const int64_t batch_idx = blockIdx.x;
         const int64_t seq_idx = blockIdx.y;
+        const int64_t tid = blockIdx.z * TPB + threadIdx.x;
 
-        const int64_t token_idx = seqstarts[batch_idx] + seq_idx;
-        const int64_t pos_idx = seq_idx + start_pos[batch_idx];
-        auto q_ptr = query + token_idx * query_stride_s;
-        auto k_ptr = key + token_idx * key_stride_s;
-        auto rq_ptr = rotated_query + token_idx * rotated_query_stride_s;
-        auto rk_ptr = rotated_key + token_idx * rotated_key_stride_s;
+        if (tid < p.num_heads * p.head_dim) {
+            const int64_t token_idx = p.seqstarts[batch_idx] + seq_idx;
+            const int64_t pos_idx = seq_idx + p.start_pos[batch_idx];
+            auto q_ptr = p.query + token_idx * p.query_stride_s;
+            auto k_ptr = p.key + token_idx * p.key_stride_s;
+            auto rq_ptr = p.rotated_query + token_idx * p.rotated_query_stride_s;
+            auto rk_ptr = p.rotated_key + token_idx * p.rotated_key_stride_s;
 
-        for (int64_t tid = threadIdx.x;
-            tid < num_heads * head_dim;
-            tid += blockDim.x)
-        {
-            const int64_t head_idx = tid / head_dim;
-            const int64_t dim_dix = tid % head_dim;
+        
+            const int64_t head_idx = tid / p.head_dim;
+            const int64_t dim_dix = tid % p.head_dim;
 
-            if (dim_dix >= rotary_dim) {
+            if (dim_dix >= p.rotary_dim) {
                 rq_ptr[tid] = q_ptr[tid];
-                if (head_idx < num_key_heads)
+                if (head_idx < p.num_key_heads)
                     rk_ptr[tid] = k_ptr[tid];
             } else {
                 const float2 q = __half22float2(q_ptr[tid]);
 
                 const float2 b = rotary_position_embedding_coeff(
-                    dim_dix, pos_idx, head_dim, theta);
+                    dim_dix, pos_idx, p.head_dim, p.theta);
 
                 rq_ptr[tid] = {
                     __float2half(q.x * b.x - q.y * b.y),
                     __float2half(q.y * b.x + q.x * b.y)
                 };
 
-                if (head_idx < num_key_heads) {
+                if (head_idx < p.num_key_heads) {
                     if (bypass_key) {
                         rk_ptr[tid] = k_ptr[tid];
                     } else {
@@ -323,15 +326,12 @@ ppl::common::RetCode dynamic_batching_rotary_position_embedding(
         return ppl::common::RC_INVALID_VALUE;
     }
 
-    const int32_t TPB = GetBlockSize(query_shape->GetDim(0) * num_heads * head_dim / 2);
-    const dim3 grid(batch, max_seqlen);
-    dynamic_batching_rotary_position_embedding_kernel<<<grid, TPB, 0, stream>>>(
-        (const half2*)query,
-        (const half2*)key,
-        (const int64_t*)start_pos,
-        (const int64_t*)seqstarts,
+    dynamic_batching_rotary_position_embedding_kernel_param p = {
+        (half2 *)query,
+        (half2 *)key,
+        (int64_t*) start_pos,
+        (int64_t*) seqstarts,
         theta,
-        bypass_key,
         rotary_dim / 2,
         num_heads,
         num_key_heads,
@@ -340,9 +340,17 @@ ppl::common::RetCode dynamic_batching_rotary_position_embedding(
         key_shape->GetDim(1) * head_dim / 2,
         rotated_query_shape->GetDim(1) * head_dim / 2,
         rotated_key_shape->GetDim(1) * head_dim / 2,
-        (half2*)rotated_query,
-        (half2*)rotated_key
-    );
+        (half2 *)rotated_query,
+        (half2 *)rotated_key,
+    };
+
+    const int32_t TPB = 256;
+    const dim3 grid(batch, max_seqlen, (num_heads * head_dim / 2 + TPB - 1) / TPB);
+    if (bypass_key) {
+        dynamic_batching_rotary_position_embedding_kernel<TPB, true><<<grid, TPB, 0, stream>>>(p);
+    } else {
+        dynamic_batching_rotary_position_embedding_kernel<TPB, false><<<grid, TPB, 0, stream>>>(p);
+    }
     
     return ppl::common::RC_SUCCESS;
 }
---
+++ src/ppl/kernel/llm/cuda/pmx/type.h
@@ -0,0 +1,51 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+#ifndef __PPL_KERNEL_LLM_CUDA_PMX_TYPE_H__
+#define __PPL_KERNEL_LLM_CUDA_PMX_TYPE_H__
+
+#include "ppl/kernel/llm/cuda/common/general_include.h"
+
+#include <cuda_runtime.h>
+#include <cuda_fp16.h>
+#include <math.h>
+
+namespace ppl { namespace kernel { namespace llm { namespace cuda { namespace pmx {
+
+using fp16_t   = half;
+using fp16x2_t = half2;
+
+using fp32_t   = float;
+using fp32x2_t = float2;
+using fp32x4_t = float4;
+
+using int8x2_t = char2;
+using int8x4_t = char4;
+
+using uint8x2_t = uchar2;
+using uint8x4_t = uchar4;
+
+using int32x4_t = int4;
+using int32x2_t = int2;
+
+using int4x8_t = int32_t;
+using int4x4_t = int16_t;
+using int4x2_t = int8_t;
+
+}}}}}
+
+#endif
