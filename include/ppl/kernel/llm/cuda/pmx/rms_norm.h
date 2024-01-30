352d5f69339f7c818b7870a312e5d313db5c1da7
alcanderian <alcanderian@gmail.com>
2024-01-31 00:05:21 +0800

    [fix] fix and move rmsnorm kernel here

---
+++ include/ppl/kernel/llm/cuda/pmx/rms_norm.h
@@ -0,0 +1,39 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+#ifndef __PPL_KERNEL_LLM_CUDA_PMX_RMSNORM_H__
+#define __PPL_KERNEL_LLM_CUDA_PMX_RMSNORM_H__
+
+#include "ppl/kernel/llm/cuda/common/general_include.h"
+
+namespace ppl { namespace kernel { namespace llm { namespace cuda { namespace pmx {
+
+ppl::common::RetCode rms_norm(
+    cudaStream_t stream,
+    const ppl::common::TensorShape* input_shape,
+    const void* input,
+    const void* skip_in,
+    const void* weight,
+    const int32_t axis,
+    const float eps,
+    const bool skip_term,
+    void* output,
+    void* skip_out);
+
+}}}}}
+
+#endif
