3eea4cd4240c5f838358ed182e3a3a06ca3639da
alcanderian <alcanderian@gmail.com>
2024-03-11 14:43:02 +0800

    [feature] add new kernel `position_index`

---
+++ include/ppl/kernel/llm/cuda/pmx/position_index.h
@@ -0,0 +1,35 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+#ifndef __PPL_KERNEL_LLM_CUDA_PMX_POSITION_INDEX_H__
+#define __PPL_KERNEL_LLM_CUDA_PMX_POSITION_INDEX_H__
+
+#include "ppl/kernel/llm/cuda/common/general_include.h"
+
+namespace ppl { namespace kernel { namespace llm { namespace cuda { namespace pmx {
+
+ppl::common::RetCode dynamic_batching_position_index(
+    const cudaStream_t stream,
+    const void* start_pos,
+    const void* seqstarts,
+    const int64_t batch,
+    const int64_t max_seqlen,
+    void* output);
+
+}}}}}
+
+#endif
