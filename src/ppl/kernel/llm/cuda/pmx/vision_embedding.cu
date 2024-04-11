3b3c5fc6b4794d0fd82df4dfae43ff0dc2e3de0d
Liheng Jian <jianliheng@sensetime.com>
2024-04-11 14:55:31 +0800

    [opt]optimize memory and cudnn handle in vision embedding.

--- src/ppl/kernel/llm/cuda/pmx/vision_embedding.cu
+++ src/ppl/kernel/llm/cuda/pmx/vision_embedding.cu
@@ -19,7 +19,6 @@
 #include "ppl/common/log.h"
 
 #include <cuda_fp16.h>
-#include <cudnn.h>
 
 namespace ppl { namespace kernel { namespace llm { namespace cuda { namespace pmx {
 
@@ -126,176 +125,41 @@ void combine_embedding(
 
 ppl::common::RetCode vision_embedding(
     const cudaStream_t stream,
-    const ppl::common::TensorShape* image_shape,  // [n, c, h, w]
+    cudnnHandle_t cudnn_handle,
+    cudnnTensorDescriptor_t image_desc,
     const void* images,
+    cudnnFilterDescriptor_t filter_desc,
+    const void* patch_emb_weight,  // weight of convolution filter
+    cudnnConvolutionDescriptor_t conv_desc,
+    void* workspace,
+    size_t workspace_size,
+    cudnnTensorDescriptor_t patch_desc0,
+    void* patch_embeds0,  // output of cudnnConvolutionForward()
+    cudnnTensorDescriptor_t patch_desc1,
+    void* patch_embeds1,  // output of cudnnTransformTensor()
     const void* cls_emb_weight,  // [hidden_dim]
-    const ppl::common::TensorShape* patch_emb_shape,  // [hidden_dim, c, patch_size, patch_size]
-    const void* patch_emb_weight,
     const void* pos_emb_weight,  // [num_positions * hidden_dim]
+    int32_t grid,
+    int32_t batch_size,
     const int32_t hidden_dim,
-    const int32_t image_size,
-    const int32_t patch_size,
-    const ppl::common::TensorShape* output_shape, // [batch_size, (image_size / patch_size) * (image_size / patch_size) + 1, hidden_dim]
     void* output)
 {
-    if (image_shape->GetDataType() != ppl::common::DATATYPE_FLOAT16) {
-        LOG(ERROR) << "vision_embedding image only support fp16, but got ["<< image_shape->GetDataType() << "]";
-        return ppl::common::RC_UNSUPPORTED;
-    }
-    if (patch_emb_shape->GetDataType() != ppl::common::DATATYPE_FLOAT16) {
-        LOG(ERROR) << "vision_embedding kernel only support fp16, but got ["<< patch_emb_shape->GetDataType() << "]";
-        return ppl::common::RC_UNSUPPORTED;
-    }
-    if (output_shape->GetDataType() != ppl::common::DATATYPE_FLOAT16) {
-        LOG(ERROR) << "vision_embedding output only support fp16, but got ["<< output_shape->GetDataType() << "]";
-        return ppl::common::RC_UNSUPPORTED;
-    }
-    if (images == nullptr) {
-        LOG(ERROR) << "invalid pointer of the image data.";
-        return ppl::common::RC_INVALID_VALUE;
-    }
-    if (cls_emb_weight == nullptr) {
-        LOG(ERROR) << "invalid pointer of the class embedding weight.";
-        return ppl::common::RC_INVALID_VALUE;
-    }
-    if (patch_emb_weight == nullptr) {
-        LOG(ERROR) << "invalid pointer of the filter weight.";
-        return ppl::common::RC_INVALID_VALUE;
-    }
-    if (pos_emb_weight == nullptr) {
-        LOG(ERROR) << "invalid pointer of the position embedding weight.";
-        return ppl::common::RC_INVALID_VALUE;
-    }
-    if (output == nullptr) {
-        LOG(ERROR) << "invalid pointer of the output.";
-        return ppl::common::RC_INVALID_VALUE;
-    }
-
-    // image convolution
     cudnnStatus_t status;
-    cudnnHandle_t cudnn_handle;
-    status = cudnnCreate(&cudnn_handle);
-    if (status != CUDNN_STATUS_SUCCESS) {
-        LOG(ERROR) << "failed to create a cudnn handle with error: " << status;
-        return ppl::common::RC_OTHER_ERROR;
-    }
-
-    const int32_t batch_size = image_shape->GetDim(0);
-    const int32_t image_channel = image_shape->GetDim(1);
-    const int32_t image_size1 = image_shape->GetDim(2);
-    if (image_size != image_size1) {
-        LOG(ERROR) << "invalid image size.";
-        return ppl::common::RC_INVALID_VALUE;
-    }
-    const int32_t grid = image_size / patch_size;
-    cudnnTensorDescriptor_t image_desc, patch_desc0, patch_desc1;
-    status = cudnnCreateTensorDescriptor(&image_desc);
-    if (status != CUDNN_STATUS_SUCCESS) {
-        LOG(ERROR) << "failed to create the tensor descriptor of image with error: " << status;
-        return ppl::common::RC_OTHER_ERROR;
-    }
-    status = cudnnCreateTensorDescriptor(&patch_desc0);
-    if (status != CUDNN_STATUS_SUCCESS) {
-        LOG(ERROR) << "failed to create the tensor descriptor of convolution output with error: " << status;
-        return ppl::common::RC_OTHER_ERROR;
-    }
-    status = cudnnCreateTensorDescriptor(&patch_desc1);
-    if (status != CUDNN_STATUS_SUCCESS) {
-        LOG(ERROR) << "failed to create the tensor descriptor of transposed convolution output with error: " << status;
-        return ppl::common::RC_OTHER_ERROR;
-    }
-    status = cudnnSetTensor4dDescriptor(image_desc, CUDNN_TENSOR_NCHW,
-             CUDNN_DATA_HALF, batch_size, image_channel, image_size, image_size);
-    if (status != CUDNN_STATUS_SUCCESS) {
-        LOG(ERROR) << "failed to set the tensor descriptor of input with error: " << status;
-        return ppl::common::RC_OTHER_ERROR;
-    }
-    status = cudnnSetTensor4dDescriptor(patch_desc0, CUDNN_TENSOR_NCHW,
-             CUDNN_DATA_HALF, batch_size, hidden_dim, grid, grid);
-    if (status != CUDNN_STATUS_SUCCESS) {
-        LOG(ERROR) << "failed to set the tensor descriptor of convolution output with error: " << status;
-        return ppl::common::RC_OTHER_ERROR;
-    }
-    status = cudnnSetTensor4dDescriptor(patch_desc1, CUDNN_TENSOR_NHWC,
-             CUDNN_DATA_HALF, batch_size, hidden_dim, grid, grid);
-    if (status != CUDNN_STATUS_SUCCESS) {
-        LOG(ERROR) << "failed to set the tensor descriptor of transposed convolution output with error: " << status;
-        return ppl::common::RC_OTHER_ERROR;
-    }
-
-    cudnnFilterDescriptor_t kernel_desc;
-    status = cudnnCreateFilterDescriptor(&kernel_desc);
-    if (status != CUDNN_STATUS_SUCCESS) {
-        LOG(ERROR) << "failed to create the filter descriptor with error: " << status;
-        return ppl::common::RC_OTHER_ERROR;
-    }
-    status = cudnnSetFilter4dDescriptor(kernel_desc, CUDNN_DATA_HALF,
-             CUDNN_TENSOR_NCHW, hidden_dim, image_channel, patch_size, patch_size);
-    if (status != CUDNN_STATUS_SUCCESS) {
-        LOG(ERROR) << "failed to set the filter descriptor with error: " << status;
-        return ppl::common::RC_OTHER_ERROR;
-    }
-
-    cudnnConvolutionDescriptor_t conv;
-    status = cudnnCreateConvolutionDescriptor(&conv);
-    if (status != CUDNN_STATUS_SUCCESS) {
-        LOG(ERROR) << "failed to create the convolution descriptor with error: " << status;
-        return ppl::common::RC_OTHER_ERROR;
-    }
-    status = cudnnSetConvolution2dDescriptor(conv, 0, 0, patch_size, patch_size, 1, 1,
-             CUDNN_CONVOLUTION, CUDNN_DATA_HALF);
-    if (status != CUDNN_STATUS_SUCCESS) {
-        LOG(ERROR) << "failed to set the convolution descriptor with error: " << status;
-        return ppl::common::RC_OTHER_ERROR;
-    }
-    status = cudnnSetConvolutionMathType(conv, CUDNN_TENSOR_OP_MATH);
-    if (status != CUDNN_STATUS_SUCCESS) {
-        LOG(ERROR) << "failed to set the convolution math type with error: " << status;
-        return ppl::common::RC_OTHER_ERROR;
-    }
-/* // come here
-    cudnnConvolutionFwdAlgoPerf_t algos[3];
-    int returnedAlgoCount;
-    status = cudnnGetConvolutionForwardAlgorithm_v7(cudnn_handle, image_desc,
-             kernel_desc, conv, patch_desc0, 3, &returnedAlgoCount, algos);
-    if (status != CUDNN_STATUS_SUCCESS) {
-        LOG(ERROR) << "failed to get the convolution algorithm with error: " << status;
-        return ppl::common::RC_OTHER_ERROR;
-    } */
-
-    size_t workspace_size;
-    status = cudnnGetConvolutionForwardWorkspaceSize(cudnn_handle, image_desc,
-             kernel_desc, conv, patch_desc0,
-             CUDNN_CONVOLUTION_FWD_ALGO_IMPLICIT_PRECOMP_GEMM,
-             &workspace_size);
-    if (status != CUDNN_STATUS_SUCCESS) {
-        LOG(ERROR) << "failed to get the workspace size of convolution with error: " << status;
-        return ppl::common::RC_OTHER_ERROR;
-    }
-
-    size_t size = batch_size * hidden_dim * grid * grid * sizeof(half);
-    void* workspace;
-    cudaMalloc(&workspace, workspace_size);
-    void* patch_embeds0;
-    void* patch_embeds1;
-    cudaMalloc(&patch_embeds0, size);
-    cudaMalloc(&patch_embeds1, size);
-
     float alpha = 1.f;
     float beta = 1.f;
     status = cudnnConvolutionForward(cudnn_handle, &alpha, image_desc,
-                 images, kernel_desc, patch_emb_weight, conv,
+                 images, filter_desc, patch_emb_weight, conv_desc,
                  CUDNN_CONVOLUTION_FWD_ALGO_IMPLICIT_PRECOMP_GEMM, workspace,
                  workspace_size, &beta, patch_desc0, patch_embeds0);
     if (status != CUDNN_STATUS_SUCCESS) {
-        LOG(ERROR) << "failed to execute convolution with error: " << status;
+        LOG(ERROR) << "failed to execute convolution with error: " << cudnnGetErrorString(status);
         return ppl::common::RC_OTHER_ERROR;
     }
 
     status = cudnnTransformTensor(cudnn_handle, &alpha, patch_desc0,
                  patch_embeds0, &beta, patch_desc1, patch_embeds1);
     if (status != CUDNN_STATUS_SUCCESS) {
-        LOG(ERROR) << "failed to transpose convolution output with error: " << status;
+        LOG(ERROR) << "failed to transpose convolution output with error: " << cudnnGetErrorString(status);
         return ppl::common::RC_OTHER_ERROR;
     }
 
@@ -303,40 +167,6 @@ ppl::common::RetCode vision_embedding(
     combine_embedding((const half*)patch_embeds1, (const half*)cls_emb_weight,
                       (const half*)pos_emb_weight, batch_size,
                       num_positions, hidden_dim, (half*)output);
-
-    cudaFree(workspace);
-    cudaFree(patch_embeds0);
-    cudaFree(patch_embeds1);
-    status = cudnnDestroyTensorDescriptor(image_desc);
-    if (status != CUDNN_STATUS_SUCCESS) {
-        LOG(ERROR) << "failed to destroy the cudnn tensor descriptor image_desc with error: " << status;
-        return ppl::common::RC_OTHER_ERROR;
-    }
-    status = cudnnDestroyTensorDescriptor(patch_desc0);
-    if (status != CUDNN_STATUS_SUCCESS) {
-        LOG(ERROR) << "failed to destroy the cudnn tensor descriptor patch_desc0 with error: " << status;
-        return ppl::common::RC_OTHER_ERROR;
-    }
-    status = cudnnDestroyTensorDescriptor(patch_desc1);
-    if (status != CUDNN_STATUS_SUCCESS) {
-        LOG(ERROR) << "failed to destroy the cudnn tensor descriptor patch_desc1 with error: " << status;
-        return ppl::common::RC_OTHER_ERROR;
-    }
-    status = cudnnDestroyFilterDescriptor(kernel_desc);
-    if (status != CUDNN_STATUS_SUCCESS) {
-        LOG(ERROR) << "failed to destroy the cudnn filter descriptor kernel_desc with error: " << status;
-        return ppl::common::RC_OTHER_ERROR;
-    }
-    status = cudnnDestroyConvolutionDescriptor(conv);
-    if (status != CUDNN_STATUS_SUCCESS) {
-        LOG(ERROR) << "failed to destroy the cudnn convolution descriptor conv with error: " << status;
-        return ppl::common::RC_OTHER_ERROR;
-    }
-    status = cudnnDestroy(cudnn_handle);
-    if (status != CUDNN_STATUS_SUCCESS) {
-        LOG(ERROR) << "failed to destroy the cudnn handle with error: " << status;
-        return ppl::common::RC_OTHER_ERROR;
-    }
 
     return ppl::common::RC_SUCCESS;
 }
