4e0020694d204a5ea20fd1915f9753e53f18cb08
Liheng Jian <jianliheng@sensetime.com>
2024-04-11 14:55:31 +0800

    [opt]optimize memory and cudnn handle in vision embedding.

--- include/ppl/kernel/llm/cuda/pmx/vision_embedding.h
+++ include/ppl/kernel/llm/cuda/pmx/vision_embedding.h
@@ -18,22 +18,31 @@
 #ifndef __PPL_KERNEL_LLM_CUDA_PMX_VISION_EMBEDDING_H__
 #define __PPL_KERNEL_LLM_CUDA_PMX_VISION_EMBEDDING_H__
 
+#include <cudnn.h>
+
 #include "ppl/kernel/llm/cuda/common/general_include.h"
 
 namespace ppl { namespace kernel { namespace llm { namespace cuda { namespace pmx {
 
 ppl::common::RetCode vision_embedding(
     const cudaStream_t stream,
-    const ppl::common::TensorShape* image_shape, //[batch_size, channels, image_size, image_size]
+    cudnnHandle_t cudnn_handle,
+    cudnnTensorDescriptor_t image_desc,
     const void* images,
-    const void* cls_emb_weight,    // [hidden_dim]
-    const ppl::common::TensorShape* patch_emb_shape, // [hidden_dim, channels, patch_size, patch_size]
+    cudnnFilterDescriptor_t filter_desc,
     const void* patch_emb_weight,
-    const void* pos_emb_weight,  // [num_positions * hidden_dim]
+    cudnnConvolutionDescriptor_t conv_desc,
+    void* workspace,
+    size_t workspace_size,
+    cudnnTensorDescriptor_t patch_desc0,
+    void* patch_embeds0,
+    cudnnTensorDescriptor_t patch_desc1,
+    void* patch_embeds1,
+    const void* cls_emb_weight,
+    const void* pos_emb_weight,
+    int32_t grid,
+    int32_t batch_size,
     const int32_t hidden_dim,
-    const int32_t image_size,
-    const int32_t patch_size,
-    const ppl::common::TensorShape* output_shape,  // [batch_size, (image_size / patch_size) * (image_size / patch_size) + 1, hidden_dim]
     void* output);
 
 }}}}}
